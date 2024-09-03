f8a498a680a3218bb26fb4f1916319249581a12d
Anyethan <1972553167@qq.com>
2024-09-02 22:08:23 -0700

    dead handler and end handler with an endpoint

--- Week02/PlatformerDemo/Assets/Scripts/PlayerController.cs
+++ Week02/PlatformerDemo/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,7 +14,9 @@ public class PlayerController : MonoBehaviour
     
     public float speed;
     public float jumpSpeed;
-    
+
+    private float delay = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +29,10 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
         DropHandler();
+        DeadHandler();
     }
+
+  
 
     void Movement()
     {
@@ -63,4 +70,22 @@ public class PlayerController : MonoBehaviour
             selfAnim.SetBool("idleFlag", true);
         }
     }
+
+    void DeadHandler()
+    {
+        if (selfRd.position[1] < -6)
+        {
+            Invoke("Reset", delay);
+            
+        }
+    }
+
+    private void Reset()
+    {
+        transform.position = new Vector3(0, 1, 0);
+    }
+
 }
+
+
+   
